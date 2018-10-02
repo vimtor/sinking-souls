77f63843418e62a69e6bef0077144f5080764763
Víctor Navarro González <victor.navarro@enti.cat>
2018-10-02 21:29:39 +0200

    Basic PCG implemented

---
+++ Sinking Souls/Assets/Scripts/Room.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room {
+
+    public enum RoomType {
+        COMBAT,
+        BOSS
+    }
+
+    public Vector2 gridPos;
+    public RoomType type;
+    public bool doorTop, doorBot, doorLeft, doorRight;
+    public GameObject prefab;
+
+	public Room(Vector2 _gridPos, RoomType _type) {
+        gridPos = _gridPos;
+        type = _type;
+    }
+
+}

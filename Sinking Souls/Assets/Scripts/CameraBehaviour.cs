dadfb85805bcae1010f41edf476c1aa5f174ab41
Víctor Navarro González <victor.navarro@enti.cat>
2018-10-09 16:20:03 +0200

    Basic isometric camera implemented
    The camera recenters to an specified point and is able to look torward the player and rotate over the map.

---
+++ Sinking Souls/Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour {
+
+    public Transform player;
+    Vector3 center, offset;
+    
+	void Start () {
+        // Setup initial center and offset values,
+        // based on how the camera is initially positioned.
+        center = new Vector3(0, 0, 0);
+        offset = transform.position - center;
+
+        SetupCenter(center);
+        transform.LookAt(center);
+    }
+	
+	void Update () {
+        // SetupCenter(player.position);
+        // RotateCamera();
+        LookPlayer();
+    }
+
+    public void SetupCenter(Vector3 newCenter) {
+        center = newCenter;
+        transform.position = center + offset;
+    }
+
+    private void LookPlayer() {
+        transform.LookAt(player.position);
+    }
+
+    private void RotateCamera() {
+        Vector3 angles = new Vector3(0, InputHandler.RightJoystick.x, 0);
+        offset = Quaternion.Euler(angles) * offset;
+        transform.position = center + offset;
+        transform.LookAt(center);
+    }
+}

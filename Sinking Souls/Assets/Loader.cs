7a6a5f865fc6c00f960a31d91a5fecda80c5008a
Marc <marc.solis@enti.cat>
2018-12-03 17:09:54 +0100

    Trial Version

--- Sinking Souls/Assets/Loader.cs
+++ Sinking Souls/Assets/Loader.cs
@@ -6,13 +6,22 @@ using UnityEngine;
 public class Loader : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-       
+	void Start () {      
         Debug.Log("DONE");
-        //GameController.instance.scene = GameController.GameState.GAME;
-        //GameController.instance.gameObject.GetComponent<LevelGenerator>().currentLevel++;
-        //Debug.Log(GameController.instance.gameObject.GetComponent<LevelGenerator>().currentLevel);
-        //SceneManager.LoadScene("Game", LoadSceneMode.Single);
     }
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameController.instance.scene = GameController.GameState.GAME;
+            GameController.instance.gameObject.GetComponent<LevelGenerator>().currentLevel++;
+            Debug.Log(GameController.instance.gameObject.GetComponent<LevelGenerator>().currentLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+    }
 }

e15a9e7876fa24e60382d1c68ab17831e7da5b2f
Unknown <arnau.aguilar@enti.cat>
2018-12-12 09:05:43 +0100

    Prefab that chooses a renadom positon created
    Next step is creating a prefab with X augmented splash abilities inside so that they choosse a random position. make a script that creates X child prefabs

---
+++ Sinking Souls/Assets/Scripts/PlugableAI/Actions/AugmentedSpellAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlugableAI/Actions/AugmentedSpell")]
+public class AugmentedSpellAction : Action {
+
+    [Range(0.0f, 1.0f)] public float actionFrame = 0.5f;
+    public int amount = 4;
+    public int range = 20;
+    private float clipLength;
+
+    public override void Act(AIController controller) {
+
+        clipLength = controller.GetComponent<Enemy>().clipLength["SpellAnim"];
+        controller.SetAnimBool("SPELL");
+        Debug.Log("hola?");
+        if (!controller.GetComponent<Enemy>().thrown && (controller.CheckIfCountDownElapsed(clipLength * actionFrame))) {
+            for (int i = 0; i < amount; i++) {
+                controller.GetComponent<Enemy>().ability.Use(controller.gameObject);
+                Debug.Log("spawn");
+            }
+        }
+
+        if (controller.CheckIfCountDownElapsed(controller.GetComponent<Enemy>().clipLength["SpellAnim"])) {
+            controller.stateTimeElapsed = 0;
+            controller.GetComponent<Enemy>().thrown = false;
+        }
+
+    }
+}

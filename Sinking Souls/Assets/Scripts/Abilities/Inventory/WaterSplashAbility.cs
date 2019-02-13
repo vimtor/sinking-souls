e5a76c10cd8e1faa0e5785c2112372e0a31d9db1
Unknown <arnau.aguilar@enti.cat>
2019-02-13 17:00:55 +0100

    Sorcerer finished

--- Sinking Souls/Assets/Scripts/Abilities/Inventory/WaterSplashAbility.cs
+++ Sinking Souls/Assets/Scripts/Abilities/Inventory/WaterSplashAbility.cs
@@ -9,11 +9,20 @@ public class WaterSplashAbility : Ability {
     public float delay;
     public float range;
     public bool pickRandomPosition;
+    public bool augmented;
 
     protected override void Configure(GameObject prefab) {
-        prefab.GetComponent<WaterSplashBehaviour>().delay = delay;
-        prefab.GetComponent<WaterSplashBehaviour>().range = range;
-        prefab.GetComponent<WaterSplashBehaviour>().pickRandomPosition = pickRandomPosition;
+        if (!augmented) {
+            prefab.GetComponent<WaterSplashBehaviour>().delay = delay;
+            prefab.GetComponent<WaterSplashBehaviour>().range = range;
+            prefab.GetComponent<WaterSplashBehaviour>().pickRandomPosition = pickRandomPosition;
+        }
+        else {
+            prefab.GetComponent<WaterSplashSpawner>().delay = delay;
+            prefab.GetComponent<WaterSplashSpawner>().range = range;
+            prefab.GetComponent<WaterSplashSpawner>().pickRandomPosition = pickRandomPosition;
+        }
+
     }
 
 }

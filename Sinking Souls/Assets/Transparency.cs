678484adae6ec88de93c63f976145e0b93e77310
Unknown <arnau.aguilar@enti.cat>
2019-02-21 11:44:00 +0100

    Wall shader finished

---
+++ Sinking Souls/Assets/Transparency.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transparency : MonoBehaviour {
+
+    public List<GameObject> objects;
+    public float FadeDistance = 4;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 player = GameController.instance.player.transform.position;
+        Vector3 camera = CameraManager.instance.virtualCamera.transform.position;
+        Vector3 pC = camera - player;
+        Debug.DrawRay(camera,player - camera, Color.red);
+        foreach (GameObject go in objects) {
+            go.GetComponent<Renderer>().material.SetVector("_PlayerPosition", player + pC.normalized * 4);
+            go.GetComponent<Renderer>().material.SetVector("_Camera", camera);
+            go.GetComponent<Renderer>().material.SetFloat("_FadeDistance", 4);
+        }
+	}
+}

5ac8ea5ee66cea36ceae0d310e97bdb89d430683
Víctor Navarro González <victor.navarro@enti.cat>
2019-03-30 19:38:43 +0100

    Basic cinematic system implemented

---
+++ Sinking Souls/Assets/Cinematics/DialoguePlayableBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class DialoguePlayableBehaviour : PlayableBehaviour
+{
+    public Dialogue dialogue;
+    public PlayableDirector director;
+    public TimelineAsset timeline;
+
+    private bool canPlayNext;
+
+    
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        Debug.Log(dialogue.name);
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            director.Play(timeline);
+        }
+    }
+
+    public override void OnGraphStart(Playable playable)
+    {
+        base.OnGraphStart(playable);
+        canPlayNext = true;
+    }
+
+    public override void OnPlayableDestroy(Playable playable)
+    {
+        base.OnPlayableDestroy(playable);
+        if (canPlayNext) director.Play(timeline);
+    }
+}

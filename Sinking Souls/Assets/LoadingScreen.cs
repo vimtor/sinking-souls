79372a9fd6d28ee9bb9ae8708824ac3dbeab15c3
Víctor Navarro González <vn4varro@gmail.com>
2019-04-07 19:57:26 +0200

    Random screen loader

---
+++ Sinking Souls/Assets/LoadingScreen.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public Sprite[] possibleImages;
+    private Image image;
+
+	void Start ()
+    {
+        image = GetComponent<Image>();
+        image.sprite = possibleImages[Random.Range(0, possibleImages.Length)];
+    }
+}

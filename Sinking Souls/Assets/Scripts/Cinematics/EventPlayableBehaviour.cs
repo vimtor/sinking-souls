ad83aa17aaddc840d15c9412bd962f29f5106420
Víctor Navarro González <vn4varro@gmail.com>
2019-04-07 18:28:00 +0200

    Klauss event at pre-boss cinematic

---
+++ Sinking Souls/Assets/Scripts/Cinematics/EventPlayableBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using System;
+
+public class EventPlayableBehaviour : PlayableBehaviour
+{
+    private bool firstFrame;
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+    {
+        if (firstFrame) return;
+
+        firstFrame = true;
+
+        var enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        var klauss = Array.Find(enemies, enemy => enemy.name.Contains("Klaus"));
+
+        klauss.GetComponent<KlausBossAI>().SetupAI();
+    }
+}

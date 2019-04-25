c5f57e8fe4225cd44b7c79e161d8833137c4e38c
Marc <marc.solis@enti.cat>
2019-04-25 19:11:38 +0200

    Chest rooms (1st approach)

---
+++ Sinking Souls/Assets/ChestBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestBehaviour : MonoBehaviour {
+
+    public GameObject Button;
+    private GameObject canvas;
+    private GameObject instantiatedButton;
+    public AnimationClip Open;
+    private bool isOpened = false;
+
+	// Use this for initialization
+	void Start () {
+        canvas = GameObject.Find("Canvas");
+        isOpened = false;
+    }
+	
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!isOpened) instantiatedButton = Instantiate(Button, canvas.transform, false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!isOpened)
+        {
+            if (InputManager.ButtonA || Input.GetKey(KeyCode.Return))
+            {
+                Debug.Log("ENTRA");
+                isOpened = true;
+                Destroy(instantiatedButton);
+                GetComponent<Animation>().clip = Open;
+                GetComponent<Animation>().Play();
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(instantiatedButton != null) Destroy(instantiatedButton);
+    }
+
+}

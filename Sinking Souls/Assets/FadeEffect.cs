0300ab6ad6405b10c48348b515d4caccfd952152
Unknown <arnau.aguilar@enti.cat>
2019-05-02 15:56:59 +0200

    triangle fixes and jack death

---
+++ Sinking Souls/Assets/FadeEffect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeEffect : MonoBehaviour {
+
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+    private float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (fadeIn) {
+            GetComponent<Image>().color = new Color(0, 0, 0, GetComponent<Image>().color.a - speed * Time.unscaledDeltaTime);
+        }
+        if (fadeOut) {
+            GetComponent<Image>().color = new Color(0, 0, 0, GetComponent<Image>().color.a + speed * Time.unscaledDeltaTime);
+            Debug.Log("Fade " + GetComponent<Image>().color.a);
+
+        }
+    }
+    public void FadeIn(float s) {
+        fadeIn = true;
+        fadeOut = false;
+
+        speed = 1/s;
+    }
+    public void FadeOut(float s) {
+        Debug.Log("Fade Out Activated");
+        fadeIn = false;
+        fadeOut = true;
+        speed = 1/s;
+    }
+}
--- Sinking Souls/Assets/selectionTriangleBehaviour.cs
+++ Sinking Souls/Assets/selectionTriangleBehaviour.cs
@@ -21,7 +21,7 @@ public class selectionTriangleBehaviour : MonoBehaviour {
         GetComponent<MeshRenderer>().enabled = true;
 
         if (GameController.instance.player.GetComponent<Player>().lockedEnemy != null) {
-            transform.position = GameController.instance.player.GetComponent<Player>().lockedEnemy.transform.position + Vector3.up * (height + Mathf.Sin(Time.time * floatingSpeed) * floatingAmount);
+            transform.position = GameController.instance.player.GetComponent<Player>().lockedEnemy.transform.position + Vector3.up * ((height * Mathf.Clamp(GameController.instance.player.GetComponent<Player>().lockedEnemy.transform.localScale.y - 0.2f,1.2f,10)) + Mathf.Sin(Time.time * floatingSpeed) * floatingAmount);
             transform.Rotate(new Vector3(0,0,1), rotationSpeed);
             GetComponent<Renderer>().material.color = Color.Lerp(health, nearDead, GameController.instance.player.GetComponent<Player>().map(GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().Health, 0, GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Enemy>().MaxHealth, 1, 0));
         }

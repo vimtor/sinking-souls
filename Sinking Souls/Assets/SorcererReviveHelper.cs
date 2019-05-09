ed61a03680ca88f420063c8a3c18788303fad193
Marc <marc.solis@enti.cat>
2019-05-09 16:25:49 +0200

    Sacada de bezier (Sorcerer AI)

--- Sinking Souls/Assets/SorcererReviveHelper.cs
+++ Sinking Souls/Assets/SorcererReviveHelper.cs
@@ -8,24 +8,41 @@ public class SorcererReviveHelper : MonoBehaviour {
     public GameObject distance;
     public GameObject instantiated;
     public GameObject particles;
+    public GameObject magic;
+    private GameObject castedMagic;
     public enum EnemyType{
         Mele,
         Distance
-        }
+    }
 
-    
+    private EnemyType type;
     public void revive(EnemyType en, Vector3 pos) {
         if (GetComponent<Enemy>().dead) return;
+        GetComponent<Animator>().SetTrigger("ReviveSomeone");
+
+        StartCoroutine(CastMagic(en, pos));
+    }
+    IEnumerator CastMagic(EnemyType en, Vector3 pos)
+    {
+        yield return new WaitForSecondsRealtime(1.5f);
+        magic.transform.position = gameObject.transform.position + Vector3.up * 2;
+        castedMagic = Instantiate(magic, null, false);
+        castedMagic.GetComponent<castedMagicBehaviour>().chase(pos, gameObject);
+        type = en;
+    }
+    
+    public void reviveNow(Vector3 pos)
+    {
         GameObject f = Instantiate(particles, null, false);
         Destroy(f, 5);
         f.transform.position = pos;
-        StartCoroutine(WaitAndSpawn(en, pos));
-        
+        StartCoroutine(WaitAndSpawn(pos));
     }
-    IEnumerator WaitAndSpawn(EnemyType en, Vector3 pos) {
+
+    IEnumerator WaitAndSpawn( Vector3 pos) {
         yield return new WaitForSecondsRealtime(0.2f);
         GameObject enemy = null;
-        switch (en) {
+        switch (type) {
             case EnemyType.Mele:
             melee.transform.position = Vector3.zero;
             enemy = Instantiate(melee, GameController.instance.currentRoom.transform, false);
---
+++ Sinking Souls/Assets/castedMagicBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class castedMagicBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private Vector3 destination;
+    private bool go;
+    private GameObject parent;
+
+    public float flyingSpeed;
+    public float relationAtenuation;
+    private Vector2 maxHeightPoint;
+
+
+    public void chase(Vector3 pos, GameObject p)
+    {
+        destination = pos;
+        go = true;
+        parent = p;
+        maxHeightPoint = new Vector2(((destination - parent.transform.position).magnitude/2f), destination.y + 5);
+    }
+    // Update is called once per frame
+    void Update () {
+        if (go)
+        {
+            GetComponent<Rigidbody>().velocity =
+                    (destination - transform.position).normalized *
+                    flyingSpeed * ((transform.position - destination).magnitude / relationAtenuation);
+
+            Vector2 b = new Vector2(0, transform.position.y);
+            Vector2 end = new Vector2((destination - parent.transform.position).magnitude, destination.y);
+            float t = (parent.transform.position - transform.position).magnitude;
+            t = GameController.instance.player.GetComponent<Player>().map(t, 0, (destination - parent.transform.position).magnitude, 0, 1);
+            float y = ((Mathf.Pow((1 - t), 2) * b) + (2 * (1 - t) * t * maxHeightPoint) + (Mathf.Pow(t, 2) * end)).y;
+
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+
+            if(Vector3.Distance(transform.position, destination) < 0.3f)
+            {
+                parent.GetComponent<SorcererReviveHelper>().reviveNow(destination);
+                Destroy(gameObject);
+            }
+        }
+	}
+}

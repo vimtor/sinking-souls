37f383d81a5d9a7e71db129fa9e276916b22bb22
Unknown <arnau.aguilar@enti.cat>
2019-05-09 08:44:51 +0200

    Chest and skeleton melee fix

---
+++ Sinking Souls/Assets/ChestTextController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChestTextController : MonoBehaviour {
+    public string KeyBoard;
+    public string controller;
+
+	// Use this for initialization
+	void Start () {
+		if(InputManager.Xbox_One_Controller > 0) {
+            GetComponent<TextMeshPro>().text = controller;
+        }else GetComponent<TextMeshPro>().text = KeyBoard;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Sinking Souls/Assets/popUpEffect.cs
+++ Sinking Souls/Assets/popUpEffect.cs
@@ -2,27 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class popUpEffect : MonoBehaviour {
+    bool isImage;
     Image image;
+    TextMeshPro text;
     public float apearingSpeed = 30;
     bool fadeOut = false;
 	// Use this for initialization
 	void Start () {
-        image = GetComponent<Image>();
+        if (GetComponent<Image>()) {
+            image = GetComponent<Image>();
+            isImage = true;
+        }
+        else {
+            text = GetComponent<TextMeshPro>();
+            isImage = false;
+        }
+        if(isImage)
         image.color = image.color * new Vector4(1, 1, 1, 0);
-	}
+        else {
+            text.color = text.color * new Vector4(1, 1, 1, 0);
+
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (fadeOut) {
-            image.color = new Vector4(image.color.r, image.color.g, image.color.b, image.color.a - apearingSpeed * Time.deltaTime);
-            if (image.color.a <= 0) Destroy(gameObject);
+        if (isImage) {
+            if (fadeOut) {
+                image.color = new Vector4(image.color.r, image.color.g, image.color.b, image.color.a - apearingSpeed * Time.deltaTime);
+                if (image.color.a <= 0) Destroy(gameObject);
+            }
+            else {
+                image.color = new Vector4(image.color.r, image.color.g, image.color.b, image.color.a + apearingSpeed * Time.deltaTime);
+
+            }
         }
         else {
-            image.color = new Vector4(image.color.r, image.color.g, image.color.b, image.color.a + apearingSpeed * Time.deltaTime);
+            if (fadeOut) {
+                text.color = new Vector4(text.color.r, text.color.g, text.color.b, text.color.a - apearingSpeed * Time.deltaTime);
+                if (text.color.a <= 0) Destroy(gameObject);
+            }
+            else {
+                text.color = new Vector4(text.color.r, text.color.g, text.color.b, text.color.a + apearingSpeed * Time.deltaTime);
 
+            }
         }
+
     }
 
     public void destroy() {

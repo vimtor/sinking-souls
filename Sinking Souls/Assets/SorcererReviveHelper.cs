d5aad50fd15c516b68020be104514b218d262336
Unknown <arnau.aguilar@enti.cat>
2019-05-15 08:48:11 +0200

    Augmented Skeleton Sorcerer

--- Sinking Souls/Assets/SorcererReviveHelper.cs
+++ Sinking Souls/Assets/SorcererReviveHelper.cs
@@ -7,45 +7,120 @@ public class SorcererReviveHelper : MonoBehaviour {
     public GameObject melee;
     public GameObject distance;
     public GameObject instantiated;
-    public GameObject particles;
-    public GameObject magic;
+    public GameObject particles;//explosion
+    public GameObject magic;//trail
     private GameObject castedMagic;
+    [Header("Augmented")]
+    public bool augmented;
+    public GameObject augmentedMagic;//trails
+    public GameObject augmentedParticles;//explosions
+    public float closeDistance;
+    public float time;
+
+    private float timer = 0;
+    bool playerCloseDuringTime() {
+        if (Vector3.Distance(GameController.instance.player.transform.position, gameObject.transform.position) < closeDistance) timer += Time.deltaTime;
+        else timer = 0;
+        if((timer >= time)) {
+            timer = 0;
+            return true;
+        }
+        return false;
+    }
+
+
+    public void Update() {
+        if (augmented) {
+
+            if (playerCloseDuringTime()) {
+                GameObject holder = null;
+                //gets a mele if available and if not a archer
+                foreach (GameObject enem in GameController.instance.roomEnemies) {
+                    if (enem.GetComponent<AIController>().aiActive) {
+                        if (gameObject.name.Contains("Melee")) {
+                            holder = enem;
+                            break;
+                        }
+                        else {
+                            if (enem != gameObject) {
+                                holder = enem;
+                            }
+                        }
+                    }
+                }
+
+                if (holder == null) return;
+                //spawn effects and tp 
+                GameObject f = Instantiate(augmentedParticles, null, false);
+                Destroy(f, 5);
+                f.transform.position = gameObject.transform.position; //mine
+                GameObject efe = Instantiate(augmentedParticles, null, false);
+                Destroy(efe, 5);
+                efe.transform.position = holder.transform.position; //other
+
+                StartCoroutine(SwapPositions(holder));
+
+                augmentedMagic.transform.position = gameObject.transform.position + Vector3.up * 2;
+                GameObject augmCastedMagic = Instantiate(augmentedMagic, null, false);
+                augmCastedMagic.GetComponent<castedMagicBehaviour>().chase(holder.transform.position, gameObject, true);///from me to toher
+
+                augmentedMagic.transform.position = holder.transform.position + Vector3.up * 2;
+                GameObject augmCastedMagic1 = Instantiate(augmentedMagic, null, false);
+                augmCastedMagic1.GetComponent<castedMagicBehaviour>().chase(gameObject.transform.position, gameObject, true);///crida a 3
+            }
+        }
+    }
+
+
     public enum EnemyType{
         Mele,
         Distance
     }
 
     private EnemyType type;
-    public void revive(EnemyType en, Vector3 pos) {
-        if (GetComponent<Enemy>().dead) return;
-        GetComponent<Animator>().SetTrigger("ReviveSomeone");
 
-        StartCoroutine(CastMagic(en, pos));
+    public void revive(EnemyType en, Vector3 pos) { //1
+        if (GetComponent<Enemy>().dead) return;
+        
+            GetComponent<Animator>().SetTrigger("ReviveSomeone");
+
+            StartCoroutine(CastMagic(en, pos));
+     
+
     }
-    IEnumerator CastMagic(EnemyType en, Vector3 pos)
+    IEnumerator SwapPositions(GameObject holder) {
+        yield return new WaitForSecondsRealtime(0.5f);
+        Vector3 help = holder.transform.position;
+        holder.transform.position = transform.position;
+        transform.position = help;
+    }
+
+    IEnumerator CastMagic(EnemyType en, Vector3 pos)//2
     {
         yield return new WaitForSecondsRealtime(1.5f);
         magic.transform.position = gameObject.transform.position + Vector3.up * 2;
         castedMagic = Instantiate(magic, null, false);
-        castedMagic.GetComponent<castedMagicBehaviour>().chase(pos, gameObject);
+        castedMagic.GetComponent<castedMagicBehaviour>().chase(pos, gameObject);///crida a 3
         type = en;
     }
     
-    public void reviveNow(Vector3 pos)
+    public void reviveNow(Vector3 pos)//3
     {
         GameObject f = Instantiate(particles, null, false);
         Destroy(f, 5);
         f.transform.position = pos;
         StartCoroutine(WaitAndSpawn(pos));
+
     }
 
-    IEnumerator WaitAndSpawn( Vector3 pos) {
+    IEnumerator WaitAndSpawn( Vector3 pos) {//4
         yield return new WaitForSecondsRealtime(0.2f);
         GameObject enemy = null;
         switch (type) {
             case EnemyType.Mele:
             melee.transform.position = Vector3.zero;
             enemy = Instantiate(melee, GameController.instance.currentRoom.transform, false);
+            Debug.Log("Created a melee by magic");
             if (enemy.tag == "Enemy") GameController.instance.roomEnemies.Add(enemy);
             enemy.GetComponent<AIController>().SetupAI();
             enemy.transform.position = pos;

60cda255eed029de3deaef90a77a6945c117be42
Marc <marc.solis@enti.cat>
2019-05-24 13:06:07 +0200

    Lock Icon colors

--- Sinking Souls/Assets/selectionTriangleBehaviour.cs
+++ Sinking Souls/Assets/selectionTriangleBehaviour.cs
@@ -8,7 +8,8 @@ public class selectionTriangleBehaviour : MonoBehaviour {
     public float floatingAmount;
     public float rotationSpeed;
     public float height;
-    public Color health;
+    public Color maxHealth;
+    public Color midHealth;
     public Color nearDead;
 
 
@@ -21,11 +22,22 @@ public class selectionTriangleBehaviour : MonoBehaviour {
 	void Update () {
         GetComponent<MeshRenderer>().enabled = true;
 
+
+        
+
         if (GameController.instance.player.GetComponent<Player>().lockedEnemy != null) {
 
             transform.position = GameController.instance.player.GetComponent<Player>().lockedEnemy.transform.Find("SelectedPoint").transform.position + (Vector3.up * (height + (Mathf.Sin(Time.time * floatingSpeed) * floatingAmount)));
             transform.Rotate(new Vector3(0,0,1), rotationSpeed);
-            GetComponent<Renderer>().material.color = Color.Lerp(health, nearDead, GameController.instance.player.GetComponent<Player>().map(GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().Health, 0, GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Enemy>().MaxHealth, 1, 0));
+            if(GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().Health >= GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().MaxHealth / 2f)
+            {
+                //Green to Orange (100 to 50%)
+                GetComponent<Renderer>().material.color = Color.Lerp(maxHealth, midHealth, GameController.instance.player.GetComponent<Player>().map(GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().Health, GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Enemy>().MaxHealth/2f, GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Enemy>().MaxHealth, 1, 0));
+            }
+            else{
+                //Orange to Red (50 to 0%)
+                GetComponent<Renderer>().material.color = Color.Lerp(midHealth, nearDead, GameController.instance.player.GetComponent<Player>().map(GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Entity>().Health, 0, GameController.instance.player.GetComponent<Player>().lockedEnemy.GetComponent<Enemy>().MaxHealth/2f, 1, 0));
+            }
         }
         else {
             GetComponent<MeshRenderer>().enabled = false;
